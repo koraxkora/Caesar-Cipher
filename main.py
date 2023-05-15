c9ce0554ecd448174affb31bbb06125d9692a3a8
kora <super.koraxkora@gmail.com>
2023-05-15 16:59:19 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+text = input("Type your message:\n").lower()
+method = input("Type 'e' to encrypt, type 'd' to decrypt:\n")
+shift = int(input("Type the shift number:\n"))
