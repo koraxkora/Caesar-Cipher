f108796c87febd9e0e7fa15709ce1542033f258d
kora <super.koraxkora@gmail.com>
2023-05-15 22:25:24 +0200

    Add file art.py

---
+++ art.py
@@ -0,0 +1,17 @@
+logo = """           
+ ,a=====a, ,adPPYYba,  ,adPPYba, ,ad----a, ,adPPYYba, ||,dPPYba,  
+a8"     aa ""     `Y8 a8P_____88 I8[    "" ""     `Y8 ||P'   "||  
+8b         ,a------88 8PP-------  `"----,  ,a------88 ||          
+"8a,    aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 ||          
+ `"====="' `"8bbdP"Y8  `"Ybbd8"' `"-----"' `"8bbdP"Y8 ||   
+            88             88                                 
+           ""             ||                                 
+                          ||                                 
+ ,adPPYba, 88 ||,dPPYba,  ||,dPPYba,   ,adPPYba, ||,dPPYba,  
+a8"     "" 88 ||P'    "8a ||P'    "8a a8P_____88 ||P'   "||  
+8b         88 ||       d8 ||       88 8PP------- ||          
+"8a,   ,aa 88 ||b,   ,a8" ||       88 "8b,   ,aa ||          
+ `"Ybbd8"' 88 ||`YbbdP"'  ||       88  `"Ybbd8"' ||          
+              ||                                             
+              ||           
+"""
