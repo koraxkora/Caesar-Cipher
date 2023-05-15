1156161eb66f9559ce7270e112c56ce06c91e199
kora <super.koraxkora@gmail.com>
2023-05-15 17:06:54 +0200

    Add encrypt function

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 text = input("Type your message:\n").lower()
 method = input("Type 'e' to encrypt, type 'd' to decrypt:\n")
 shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    """takes the 'text' and shift each letter of the 'text' forwards in the
+    alphabet by the shift amount
+    """
+    encoded_text = ''
+    for letter in text:
+        letter_index = alphabet.index(letter)
+        new_index = letter_index + shift
+        try:
+            new_letter = alphabet[new_index]
+        except IndexError:
+            new_letter = alphabet[new_index % len(alphabet)]
+        encoded_text += new_letter
+    return encoded_text
