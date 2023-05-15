1c44572050292dcb5a7378f51225cdae04ba1dfa
kora <super.koraxkora@gmail.com>
2023-05-16 01:40:19 +0200

    Add while loop

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-text = input("Type your message:\n").lower()
-method = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-shift = int(input("Type the shift number:\n"))
 
-
-def encrypt_decrypt(cipher_method = method, text_message = text, shift_amount = shift):
+def encrypt_decrypt(cipher_method, text_message, shift_amount):
     """takes the 'text' and shift each letter of the 'text' forwards or
     backwards in the alphabet by the shift amount
     """
@@ -14,12 +14,28 @@ def encrypt_decrypt(cipher_method = method, text_message = text, shift_amount = 
     if cipher_method == 'decode':
         shift_amount *= -1
     for letter in text_message:
-        letter_index = alphabet.index(letter)
-        new_index = letter_index + shift_amount
-        try:
-            new_letter = alphabet[new_index]
-        except IndexError:
-            new_letter = alphabet[new_index % len(alphabet)]
-        new_text += new_letter
-    return new_text
+        if letter in alphabet:
+            letter_index = alphabet.index(letter)
+            new_index = letter_index + shift_amount
+            try:
+                new_letter = alphabet[new_index]
+            except IndexError:
+                new_letter = alphabet[new_index % len(alphabet)]
+            new_text += new_letter
+        else:
+            new_text += letter
+    print(f'message after modification: {new_text}')
 
+
+continuation = True
+while continuation:
+    text = input("Type your message:\n").lower()
+    method = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    shift = int(input("Type the shift number:\n"))
+    encrypt_decrypt(cipher_method=method, text_message=text,
+                    shift_amount=shift)
+    print('--------------------------------------------------------------')
+    decision = input('If you want to play again type "yes" ale type "no": ')
+    if decision == 'no':
+        continuation = False
+        print('goodbye')
